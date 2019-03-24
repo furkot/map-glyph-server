02dc2f20730fc37cc4cbd40fdffdafbad1091d17
Damian Krzeminski <pirxpilot@furkot.com>
2019-03-24 11:28:21 -0600

    replace mocha/should with tape

--- Makefile
+++ Makefile
@@ -4,6 +4,6 @@ lint:
 	./node_modules/.bin/jshint *.js lib test
 
 test:
-	./node_modules/.bin/mocha --recursive --require should
+	./node_modules/.bin/tape test/*.js | ./node_modules/.bin/tap-dot
 
 .PHONY: check lint test
--- test/map-glyph-server.js
+++ test/map-glyph-server.js
@@ -2,7 +2,8 @@ process.env.MAP_GLYPH_SERVER_FONT_PATH = __dirname + '/fixtures';
 
 const request = require('supertest');
 const app = require('..');
-
+const tape = require('tape');
+const test = require('tape-promise').default(tape);
 
 /*
 75651 fixtures/Metropolis Black/0-255.pbf
@@ -11,51 +12,46 @@ const app = require('..');
 74696 fixtures/Open Sans Regular/0-255.pbf
 */
 
-describe('map-glyph-server node module', function () {
-  it('return /fonts.json', function () {
-    return request(app)
-      .get('/fonts.json')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .then(response => {
-          response.body.should.eql([
-            'Metropolis Black',
-            'Open Sans Bold',
-            'Open Sans Regular'
-          ]);
-      });
-  });
+test('return /fonts.json', function (t) {
+  return request(app)
+    .get('/fonts.json')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .then(response => t.same(response.body, [
+          'Metropolis Black',
+          'Open Sans Bold',
+          'Open Sans Regular'
+    ]));
+});
 
-  it('return a specific font', function () {
-    return request(app)
-      .get('/fonts/Metropolis%20Black/0-255.pbf')
-      .expect('Content-Type', 'application/x-protobuf')
-      .expect('Content-Length', '75651')
-      .expect(200);
-  });
+test('return a specific font', function () {
+  return request(app)
+    .get('/fonts/Metropolis%20Black/0-255.pbf')
+    .expect('Content-Type', 'application/x-protobuf')
+    .expect('Content-Length', '75651')
+    .expect(200);
+});
 
-  it('return a fallback font', function () {
-    return request(app)
-      .get('/fonts/Open%20Sans%20Bold/256-511.pbf')
-      .expect('Content-Type', 'application/x-protobuf')
-      .expect('Content-Length', '45992')
-      .expect(200);
-  });
+test('return a fallback font', function () {
+  return request(app)
+    .get('/fonts/Open%20Sans%20Bold/256-511.pbf')
+    .expect('Content-Type', 'application/x-protobuf')
+    .expect('Content-Length', '45992')
+    .expect(200);
+});
 
-  it('return a fallback font for invalid name', function () {
-    return request(app)
-      .get('/fonts/Open%20Sans%20XXXX/256-511.pbf')
-      .expect('Content-Type', 'application/x-protobuf')
-      .expect('Content-Length', '45992')
-      .expect(200);
-  });
+test('return a fallback font for invalid name', function () {
+  return request(app)
+    .get('/fonts/Open%20Sans%20XXXX/256-511.pbf')
+    .expect('Content-Type', 'application/x-protobuf')
+    .expect('Content-Length', '45992')
+    .expect(200);
+});
 
-  it('combines fonts if needed', function() {
-    return request(app)
-      .get('/fonts/Metropolis%20Black,Open%20Sans%20Bold/0-255.pbf')
-      .expect('Content-Type', 'application/x-protobuf')
-      .expect(200);
-  });
-
+test('combines fonts if needed', function() {
+  return request(app)
+    .get('/fonts/Metropolis%20Black,Open%20Sans%20Bold/0-255.pbf')
+    .expect('Content-Type', 'application/x-protobuf')
+    .expect(200);
 });
