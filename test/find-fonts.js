735c83aa904d0e009b80f9f54deb3b8a476646c9
Damian Krzeminski <pirxpilot@furkot.com>
2024-01-12 17:21:44 +0100

    modernize find-fonts

--- test/find-fonts.js
+++ test/find-fonts.js
@@ -2,14 +2,11 @@ const test = require('node:test');
 const assert = require('node:assert/strict');
 const findFonts = require('../lib/find-fonts');
 
-test('find all fonts directories', function (t, done) {
-  findFonts(`${__dirname}/fixtures`, function(err, fonts) {
-    assert.ifError(err);
-    assert.deepEqual(fonts, {
-      'Metropolis Black': true,
-      'Open Sans Bold': true,
-      'Open Sans Regular': true
-    });
-    done();
+test('find all fonts directories', async function () {
+  const fonts = await findFonts(`${__dirname}/fixtures`);
+  assert.deepEqual(fonts, {
+    'Metropolis Black': true,
+    'Open Sans Bold': true,
+    'Open Sans Regular': true
   });
 });
